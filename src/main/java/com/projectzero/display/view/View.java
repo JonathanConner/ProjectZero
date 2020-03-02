0318c191a4f5b266f10248461941b0d1c58e34a9
Jonathan Conner <jonathan.g.conner@gmail.com>
2020-03-02 18:46:17 -0500

    Added DAO Interface

--- src/main/java/com/projectzero/display/view/View.java
+++ src/main/java/com/projectzero/display/view/View.java
@@ -1,7 +1,12 @@
 package com.projectzero.display.view;
 
+import com.projectzero.dao.DAOInterface;
+
 public abstract class View {
 
+	
+	private DAOInterface dao;
+	
 	public abstract void show();
 	
 }
